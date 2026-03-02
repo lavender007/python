09dff737920b3b6b3ec6d6f8634f9b1c764a6b48
Samantha Mayaka <mayakasamantha6@gmail.com>
2026-03-02 23:45:05 +0300

    added

---
+++ Assignment1.py
@@ -0,0 +1,20 @@
+# calculate BMI of a person who has a weight of 78 kilos and height of 1.75. BMI= (weight)/(height squared)
+
+weight= 78
+height= 1.75
+BMI= (weight/height**2)
+print("This is BMI", BMI )
+
+#Calculate the perimiter of the rectangle
+
+length = 48
+width = 25
+perimeter = ((length*2) + (width*2))
+print("This is perimeter", perimeter)
+
+#calculate area
+
+length = 48
+width = 25
+area = (length * width)
+print("This is area", area)
