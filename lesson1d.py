f4f694e88b9e261d227f8b929fccf7f07d144bcb
Samantha Mayaka <mayakasamantha6@gmail.com>
2026-03-02 13:01:29 +0300

    added

---
+++ lesson1d.py
@@ -0,0 +1,14 @@
+#create a python program that is able to calculate the simple interest given the principle as 50000, rate as 5% and time as 8 years. SI = (p * R * t)/100
+
+principle= 5000
+rate= 0.5
+time= 8
+simpleinterest= ((principle * rate * time)/100)
+print("This is the simple interest", simpleinterest)
+
+#Assinment 
+# 1.Write a python program that can be able to calculate the BMI of a person who os 78kgs and height is 1.75m. BMI = (weight)/ (height squared)
+
+#2.Find the area and perimiter of a rectangle whose length is 48cm and width is 25cm
+
+# Resarc on python list, tuple and dictionary and data types.
